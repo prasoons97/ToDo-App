8b9c57607fc8198a9788644389ce1f93804380cf
Prasoon <prasoons97@gmail.com>
2025-10-13 23:08:37 +0200

    Corinne Suggestion

--- ToDo-App.js
+++ ToDo-App.js
@@ -1,2 +1,23 @@
-//this is my main js file 
-console.log("Welcome to my ToDo App");
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+
+const addTodo = text => {
+
+    if (!text.trim()) {
+     return
+}
+
+   const li = document.createElement('li')
+    li.textContent = todoInput.value
+
+    todoList.appendChild(li);
+}
+
+todoForm.addEventListener('submit', e => {
+    e.preventDefault(); 
+
+    addTodo (todoInput.value);
+
+    todoForm.reset();
+});
